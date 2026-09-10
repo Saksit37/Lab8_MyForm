c66171b1e5864ffee0cc88faa297e96322897851
Saksit <Saksit08102548@gmail.com>
2026-09-10 22:34:50 +0700

    Exercise 1

--- lib/login_form_screen.dart
+++ lib/login_form_screen.dart
@@ -1,89 +1,89 @@
-// import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
-// class LoginFormScreen extends StatefulWidget {
-//   const LoginFormScreen({super.key});
+class LoginFormScreen extends StatefulWidget {
+  const LoginFormScreen({super.key});
 
-//   @override
-//   State<LoginFormScreen> createState() => _LoginFormScreenState();
-// }
+  @override
+  State<LoginFormScreen> createState() => _LoginFormScreenState();
+}
 
-// class _LoginFormScreenState extends State<LoginFormScreen> {
-//   // ตัวแปร State และเมธอดต่างๆ จะอยู่ที่นี่
-//   late final TextEditingController _nameController;
-//   final _formKey = GlobalKey<FormState>();
+class _LoginFormScreenState extends State<LoginFormScreen> {
+  // ตัวแปร State และเมธอดต่างๆ จะอยู่ที่นี่
+  late final TextEditingController _nameController;
+  final _formKey = GlobalKey<FormState>();
 
-//   @override
-//   void initState() {
-//     super.initState();
-//     _nameController = TextEditingController();
-//     _nameController.addListener(_printLatestValue);
-//   }
+  @override
+  void initState() {
+    super.initState();
+    _nameController = TextEditingController();
+    _nameController.addListener(_printLatestValue);
+  }
 
-//   void _printLatestValue() {
-//     print('ช่องข้อความที่สอง: ${_nameController.text}');
-//   }
+  void _printLatestValue() {
+    print('ช่องข้อความที่สอง: ${_nameController.text}');
+  }
 
-//   @override
-//   void dispose() {
-//     _nameController.dispose();
-//     super.dispose();
-//   }
+  @override
+  void dispose() {
+    _nameController.dispose();
+    super.dispose();
+  }
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(title: const Text('ฟอร์มล็อกอิน')),
-//       body: Padding(
-//         padding: const EdgeInsets.all(20.0),
-//         child: Form(
-//           key: _formKey,
-//           child: Column(
-//             crossAxisAlignment: CrossAxisAlignment.stretch,
-//             children: <Widget>[
-//               const Text(
-//                 'กรุณาป้อนข้อมูลเข้าระบบ',
-//                 style: TextStyle(fontSize: 16),
-//               ),
-//               const SizedBox(height: 20),
-//               TextFormField(
-//                 controller: _nameController,
-//                 decoration: const InputDecoration(labelText: 'ชื่อ'),
-//               ),
-//               TextFormField(
-//                 decoration: const InputDecoration(labelText: 'รหัสผ่าน'),
-//                 obscureText: true,
-//                 validator: (String? value) {
-//                   if (value == null || value.isEmpty) {
-//                     return 'รหัสผ่านต้องไม่ว่าง';
-//                   }
-//                   if (value.length < 6) {
-//                     return 'รหัสผ่านต้องมีอย่างน้อย 6 ตัวอักษร';
-//                   }
-//                   return null;
-//                 },
-//               ),
-//               ElevatedButton(
-//                 onPressed: () {
-//                   if (_formKey.currentState!.validate()) {
-//                     _formKey.currentState!.save();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('ฟอร์มล็อกอิน')),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              const Text(
+                'กรุณาป้อนข้อมูลเข้าระบบ',
+                style: TextStyle(fontSize: 16),
+              ),
+              const SizedBox(height: 20),
+              TextFormField(
+                controller: _nameController,
+                decoration: const InputDecoration(labelText: 'ชื่อ'),
+              ),
+              TextFormField(
+                decoration: const InputDecoration(labelText: 'รหัสผ่าน'),
+                obscureText: true,
+                validator: (String? value) {
+                  if (value == null || value.isEmpty) {
+                    return 'รหัสผ่านต้องไม่ว่าง';
+                  }
+                  if (value.length < 6) {
+                    return 'รหัสผ่านต้องมีอย่างน้อย 6 ตัวอักษร';
+                  }
+                  return null;
+                },
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    _formKey.currentState!.save();
 
-//                     ScaffoldMessenger.of(context).showSnackBar(
-//                       SnackBar(
-//                         content: Text(
-//                           'บันทึกข้อมูลแล้ว! ชื่อ: ${_nameController.text}',
-//                         ),
-//                       ),
-//                     );
-//                   } else {
-//                     print('ฟอร์มไม่ถูกต้อง');
-//                   }
-//                 },
-//                 child: const Text('Submit'),
-//               ),
-//             ],
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text(
+                          'บันทึกข้อมูลแล้ว! ชื่อ: ${_nameController.text}',
+                        ),
+                      ),
+                    );
+                  } else {
+                    print('ฟอร์มไม่ถูกต้อง');
+                  }
+                },
+                child: const Text('Submit'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
